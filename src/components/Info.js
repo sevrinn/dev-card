e37fa3d43174bb00f7cece8cc4f3f9890ae7c9e0
Sevrinn <sevrinnsevrinn@gmail.com>
2021-12-09 07:58:38 -0800

    initial commit. project sections set up

---
+++ src/components/Info.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div>
+      <h1>Info Section Here</h1>
+    </div>
+  )
+}
+
+export default Info
+
+
---
+++ src/components/Interests.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Interests = () => {
+  return (
+    <div>
+      <h1>Interests Section Here</h1>
+    </div>
+  )
+}
+
+export default Interests
+
