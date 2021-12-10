065e867c87240aaa6769d85e678107142170c4d9
Sevrinn <sevrinnsevrinn@gmail.com>
2021-12-09 16:17:07 -0800

    info component base added

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { FaEnvelope, FaLinkedin } from 'react-icons/fa'
+import profilePic from '../images/profileImage.jpeg'
 const Info = () => {
   return (
     <div>
-      <h1>Info Section Here</h1>
+      <img src={profilePic} className="profile-pic" alt="Sev looking cute as hell. lol" />
+      <h1>Sevrinn Welker</h1>
+      <h2>Fullstack Developer</h2>
+      <a href="https://www.sevwelker.com">sevwelker.com</a>
+      <div className="buttons">
+        <button className="btn email">
+          <FaEnvelope/> Email
+        </button>
+        <button className="btn email">
+          <FaLinkedin/> LinkedIn
+        </button>
+      </div>
     </div>
   )
 }
