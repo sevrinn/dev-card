42ee3cd43cb51a4c1cf8357adc628aeca36178a7
Sevrinn <sevrinnsevrinn@gmail.com>
2021-12-09 17:35:30 -0800

    changed text on About section

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <h1>About Section Here</h1>
+      <h3>About</h3>
+      <p>I'm a self-taught Software Engineer who ended up really loving to code! I enjoy learning new technologies and set aside time each week to continue to learn. I've mostly been using React, Express, MongoDB, and Node.js. I've also been learning PostgreSQL. I have additional experience in customer service, marketing, and content-creation.</p>
     </div>
   )
 }
